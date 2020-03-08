e459e654d2958fefbcc080fd97f27871e0ebaa4d
hippalus <habiphakan.isler@gmail.com>
2020-03-08 04:44:08 +0300

    [Docker] docker kafka configurations added

--- courier-geo-locations/src/main/java/com/courier/geolocations/utils/JsonUtils.java
+++ courier-geo-locations/src/main/java/com/courier/geolocations/utils/JsonUtils.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
